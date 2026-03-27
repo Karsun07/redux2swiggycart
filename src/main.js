7202ece48f806bae7ebb8fa0222460cee382aab7
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-27 22:33:26 +0530

    first

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App(){
+    <>
+    <Header/>
+    <Footer/>
+    </>
+}
